7292ed07ea9aca3554897c889890ebc3c8bd54ae
Donatellh2o <104277659+Donatellh2o@users.noreply.github.com>
2022-04-24 13:24:57 +0200

    Add files via upload

---
+++ Vendredi.js
@@ -0,0 +1,13 @@
+//On crée une date
+let date1 = new Date();
+
+let dateLocale = date1.toLocaleString('fr-FR',{
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'});
+
+document.getElementById('p1').innerHTML = dateLocale;
